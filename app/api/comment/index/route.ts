c826fbc3a58e577d7a5c88a85b2617661e7ad50e
Richard Bunker <32656066+richardbunker@users.noreply.github.com>
2023-07-11 18:57:59 +1000

    Update route.ts

--- app/api/comment/index/route.ts
+++ app/api/comment/index/route.ts
@@ -2,20 +2,22 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const postId = searchParams.get("postId");
-  if (isNaN(Number(postId))) {
-    return NextResponse.json(
-      { err: true, message: "Invalid postId. Must be valid integer." },
-      { status: 400 }
-    );
-  }
-  const comments = await prisma.comment.findMany({
-    where: { postId: Number(postId), parentId: null },
-    include: {
-      replies: { include: { user: { select: { username: true } } } },
-      user: { select: { username: true } },
-    },
-  });
-  return NextResponse.json({ err: false, data: comments });
+  return NextResponse.json({ err: false, data: "test" });
+
+  // const { searchParams } = new URL(request.url);
+  // const postId = searchParams.get("postId");
+  // if (isNaN(Number(postId))) {
+  //   return NextResponse.json(
+  //     { err: true, message: "Invalid postId. Must be valid integer." },
+  //     { status: 400 }
+  //   );
+  // }
+  // const comments = await prisma.comment.findMany({
+  //   where: { postId: Number(postId), parentId: null },
+  //   include: {
+  //     replies: { include: { user: { select: { username: true } } } },
+  //     user: { select: { username: true } },
+  //   },
+  // });
+  // return NextResponse.json({ err: false, data: comments });
 }
