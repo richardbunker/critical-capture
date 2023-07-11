9222fb516cd17d9ce238e88361bfbf8826f5cfc6
Richard Bunker <32656066+richardbunker@users.noreply.github.com>
2023-07-11 19:06:14 +1000

    Update route.ts

--- app/api/comment/index/route.ts
+++ app/api/comment/index/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import Error from "next/error";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -10,12 +11,16 @@ export async function GET(request: Request) {
       { status: 400 }
     );
   }
-  const comments = await prisma.comment.findMany({
-    where: { postId: Number(postId), parentId: null },
-    include: {
-      replies: { include: { user: { select: { username: true } } } },
-      user: { select: { username: true } },
-    },
-  });
-  return NextResponse.json({ err: false, data: comments });
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { postId: Number(postId), parentId: null },
+      include: {
+        replies: { include: { user: { select: { username: true } } } },
+        user: { select: { username: true } },
+      },
+    });
+    return NextResponse.json({ err: false, data: comments });
+  } catch (error) {
+    return NextResponse.json({ err: true, data: error });
+  }
 }
